ad8c2b635e1c48bd233a8f5e2ed0d7fc2ff01dae
forstmeister <c.foerster@tutanota.com>
2024-04-26 12:54:59 +0200

    added about me and my skills secttions / components  🔥

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AtfsectionComponent } from './atfsection/atfsection.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { MyskillsComponent } from './myskills/myskills.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,8 @@ import { AtfsectionComponent } from './atfsection/atfsection.component';
     RouterOutlet,
     NavigationComponent,
     AtfsectionComponent,
+    AboutmeComponent,
+    MyskillsComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ src/app/myskills/myskills.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myskills',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './myskills.component.html',
+  styleUrl: './myskills.component.scss',
+})
+export class MyskillsComponent {}
