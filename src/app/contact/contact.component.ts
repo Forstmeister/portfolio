ab9bc8ec8ba94f3f731ac6f1c6d86ba746e54263
forstmeister <c.foerster@tutanota.com>
2024-05-03 12:57:37 +0200

    added email validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule, input } from '@angular/core';
-import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
 import {
   FormControl,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  NgForm,
+  EmailValidator,
 } from '@angular/forms';
 
 @Component({
@@ -23,13 +31,24 @@ export class ContactComponent {
     checkbox: false,
   };
 
+  @ViewChild('userForm') userForm!: NgForm;
+
   nameValueEntered: boolean = false;
   emailValueEntered: boolean = false;
   messageValueEntered: boolean = false;
+  validEmail: boolean = false;
 
   submitForm(form: any): void {
     if (form.valid) {
       console.log('Form data:', this.userDetails);
+    }
+  }
+
+  isEmailValid(): any {
+    if (this.userForm.controls['email']['status'] == 'VALID') {
+      this.validEmail = true;
+    } else if (this.userForm.controls['email']['status'] == 'INVALID') {
+      this.validEmail = false;
     }
   }
 
@@ -44,6 +63,8 @@ export class ContactComponent {
       this.emailValueEntered = true;
       if (this.userDetails.email.length > 0) {
         this.emailValueEntered = false;
+        this.isEmailValid();
+        console.log(this.validEmail);
       }
     }
     if (value === 'message') {
