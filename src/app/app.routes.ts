a9b23c0a69043874ec03be64da79360efdf62897
forstmeister <c.foerster@tutanota.com>
2024-05-21 16:34:36 +0200

    edited imprint, routes and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { ImprintComponent } from './landingpage/imprint/imprint.component';
+import { PrivacyPolicyComponent } from './landingpage/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LandingpageComponent,
+  },
+  {
+    path: 'imprint',
+    component: ImprintComponent,
+  },
+  {
+    path: 'privacy-policy',
+    component: PrivacyPolicyComponent,
   },
 ];
