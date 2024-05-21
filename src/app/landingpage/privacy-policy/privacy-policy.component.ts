f051a55c671ee7b838d086ed4c23cc6214b1fbe8
Forstmeister <c.foerster@tutanota.com>
2024-05-21 19:55:07 +0200

    edited footer / header / footer

--- src/app/landingpage/privacy-policy/privacy-policy.component.ts
+++ src/app/landingpage/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from '../../shared/navigation/navigation.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [NavigationComponent, FooterComponent],
   templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss'
+  styleUrl: './privacy-policy.component.scss',
 })
-export class PrivacyPolicyComponent {
-
-}
+export class PrivacyPolicyComponent {}
