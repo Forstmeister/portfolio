e9c6efb5db8d2e9969bdf658741773f01fe8ddf1
Forstmeister <c.foerster@tutanota.com>
2024-05-25 10:15:11 +0200

    changed min length values in message and name / added new translations

--- src/app/landingpage/contact/contact.component.ts
+++ src/app/landingpage/contact/contact.component.ts
@@ -128,7 +128,7 @@ export class ContactComponent {
     }
     if (value === 'message') {
       this.messageValueEntered = true;
-      if (this.userDetails.message && this.userDetails.message.length > 4) {
+      if (this.userDetails.message && this.userDetails.message.length > 20) {
         this.messageValueEntered = false;
       }
     }
